bd93e02b62f1919f5325b0f1ff278d186eefd712
Aayushijambhulkar <aayushijambhulkar1@gmail.com>
2024-06-16 20:59:14 +0530

    Add files via upload

---
+++ sql 1 project.sql
@@ -0,0 +1,55 @@
+create table shops (
+	id int primary key,
+	name varchar (150),
+	n0_of_items int,
+	category varchar (150),
+	n0_of_emp int,
+	owner_name varchar(150),
+	address varchar(150),
+	mobile_no int,
+	sales int,
+	revenue int,
+	its_active boolean
+)
+	
+select * from shops
+insert into shops values (1,'rajkamal', 100 ,'retail',20,'rajkamal','chandni chauk',1234,123,15,true)
+insert into shops values (2,'munna',101,'online',21,'munnabhai','chhatrapati chauk',1235,124,16,false)
+insert into shops values (3,'raju',102,'wholesale',22,'rajesh','sambhaji nagar',1236,125,17,true)
+insert into shops values (4,'riyansh',103,'online',23,'riyansh','ramtek nagri',1237,126,18,false)
+insert into shops values (5,'cookies',104,'retail',24,'ramesh','sambhaji nagar',1238,127,19,false)
+insert into shops values (6,'candy crush',105,'wholesale',25,'akshay','ujwal nagar',1239,128,20,true)
+insert into shops values (7,'cotton candy',106,'online',26,'raju','jhansi rani chauk',1240,129,21,false)
+insert into shops values (8,'rani',107,'wholesale',27,'trupti','pratap nagar',1241,130,22,true)
+insert into shops values (9,'haldiram sweets',108,'online',28,'mukesh','chhatrapati chauk',1242,131,23,false)
+insert into shops values (10,'bikaner',109,'wholesale',29,'akash','sambhaji nagar',1243,132,24,true)
+insert into shops values (11,'dmart',110,'online',30,'radhika','ramtek nagri',1244,133,25,false)
+insert into shops values (12,'sadar bazar',111,'retail',31,'priyansh','sambhaji nagar',1245,134,26,false)
+insert into shops values (13,'cotton market',112,'wholesale',32,'nikhil','ujwal nagar',1246,135,27,true)
+insert into shops values (14,'jiya pan palace',113,'online',33,'jiya','jhansi rani chauk',1247,136,28,false)
+insert into shops values (15,'jayesh',114,'wholesale',34,'jayesh','pratap nagar',1248,137,29,true)
+insert into shops values (16,'sunday market',115,'online',35,'aslam bhai','chhatrapati chauk',1249,138,30,false)
+insert into shops values (17,'jamnagar wale',116,'wholesale',36,'jamesh bhai','sambhaji nagar',1250,139,31,true)
+insert into shops values (18,'prince',117,'online',37,'riyansh','ramtek nagri',1251,140,32,false)
+insert into shops values (19,'chocolate alchemy',118,'retail',38,'titu jamkar','sambhaji nagar',1252,141,33,false)
+insert into shops values (20,'sugar rush',119,'wholesale',39,'abhay','ujwal nagar',1253,142,34,true)
+insert into shops values (21,'sweety treats',120,'online',40,'ajayu','jhansi rani chauk',1254,143,35,false)
+insert into shops values (22,'heavenly delights',121,'wholesale',41,'vijay','pratap nagar',1255,144,36,true)
+insert into shops values (23,'sweet sensation',122,'online',42,'alfanshu','chhatrapati chauk',1256,145,37,false)
+insert into shops values (24,'baked bliss',123,'wholesale',43,'siddharth','sambhaji nagar',1257,146,37,true)
+insert into shops values (25,'blissful bites',124,'online',44,'shubham watkar','ramtek nagri',1258,147,38,false)
+insert into shops values (26,'candy chestr',125,'retail',45,'bunty','sambhaji nagar',1259,148,39,false)
+insert into shops values (27,'chocobliss',126,'wholesale',46,'pintu','ujwal nagar',1260,149,40,true)
+insert into shops values (28,'decadent delights',127,'online',47,'rohini','jhansi rani chauk',1261,150,41,false)
+insert into shops values (29,'dessert heaven',128,'wholesale',48,'parth','pratap nagar',1262,151,42,true)
+insert into shops values (30,'sugar and spice',129,'online',49,'jiya','jhansi rani chauk',1263,152,43,false)
+insert into shops values (31,'sugarchlemy',130,'online',50,'viju','jhansi rani chauk',1264,153,440,false)
+insert into shops values (32,'the sweet tooth',131,'wholesale',51,'anjali sharma','pratap nagar',1265,154,360,true)
+insert into shops values (33,'candied creation',132,'online',52,'sudhanshu','chhatrapati chauk',1266,155,372,false)
+insert into shops values (34,'candy cloud',133,'wholesale',53,'vinit','sambhaji nagar',1267,156,372,true)
+insert into shops values (35,'candy craving',134,'online',54,'vinesh','ramtek nagri',1268,157,385,false)
+insert into shops values (36,'candy ',135,'retail',55,'pappu','sambhaji nagar',1269,158,393,false)
+insert into shops values (37,'candy groove',136,'wholesale',56,'prakash','ujwal nagar',1270,159,480,true)
+insert into shops values (38,'caramel cottage',137,'online',57,'pakkya','jhansi rani chauk',1271,160,451,false)
+insert into shops values (39,'dessert oasis',138,'wholesale',58,'dipali','pratap nagar',1272,161,4872,true)
+insert into shops values (40,'sugar delicios',139,'online',59,'dipesh','jhansi rani chauk',1273,162,473,false)
